6b4d52ec3d2dbe45b912252e03590dfe207fe8ac
shaddow318 <stefanzwalker1@gmail.com>
2025-01-28 20:40:52 +0000

    Completed 02_06_visitreminder_ch

--- 02_library/challenges/02_06_visitreminder_ch.sql
+++ 02_library/challenges/02_06_visitreminder_ch.sql
@@ -1,2 +1,15 @@
 -- Prepare a report of the library patrons
 -- who have checked out the fewest books.
+
+SELECT
+    ptn.FirstName
+  , ptn.LastName
+  , ptn.Email
+  , (COUNT(lns.PatronID)) AS 'BookCheckoutCount'
+FROM
+  Patrons ptn
+  JOIN Loans lns
+    ON ptn.PatronID = lns.PatronID
+GROUP BY ptn.Email
+ORDER BY BookCheckoutCount
+LIMIT 5;
