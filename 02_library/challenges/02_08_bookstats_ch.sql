0d30bf5ddd9a8e6765b7a17f1f241aa87133e704
shaddow318 <stefanzwalker1@gmail.com>
2025-01-28 21:34:03 +0000

    Completed 02_08_bookstats_ch

--- 02_library/challenges/02_08_bookstats_ch.sql
+++ 02_library/challenges/02_08_bookstats_ch.sql
@@ -5,3 +5,30 @@
 
 -- Report 2: Show the five books that have been
 -- checked out the most.
+
+SELECT
+    Published
+  , (COUNT(Published)) AS PublishedCount
+FROM
+  (
+  SELECT DISTINCT
+      Title
+    , Published
+  FROM
+    Books
+  ) AS 'BooksShortlist'
+GROUP BY Published
+ORDER BY PublishedCount DESC
+;
+
+SELECT
+    bks.Title
+  , (COUNT(bks.Title)) AS CheckoutCount
+FROM
+  Books bks
+  JOIN Loans lns
+    ON bks.BookID = lns.BookID
+GROUP BY Title
+ORDER BY CheckoutCount DESC
+LIMIT 5
+;
