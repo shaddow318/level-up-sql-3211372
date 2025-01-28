7fbc6c4dd23e8137feaa1e3dc8fa62ce36f1e941
shaddow318 <stefanzwalker1@gmail.com>
2025-01-28 20:59:22 +0000

    Completed 02_07_featurebooks_ch

--- 02_library/challenges/02_07_featurebooks_ch.sql
+++ 02_library/challenges/02_07_featurebooks_ch.sql
@@ -2,3 +2,22 @@
 
 -- Make a pick list of books published from 1890-1899 
 -- which are not currently checked out.
+
+SELECT
+    Title
+  , Barcode
+  , Published
+FROM
+  Books
+WHERE
+  Published BETWEEN 1890 AND 1899
+  AND
+  BookID NOT IN (
+    SELECT
+      BookID
+    FROM 
+      Loans
+    WHERE
+      ReturnedDate IS NULL)
+ORDER BY Title
+;
